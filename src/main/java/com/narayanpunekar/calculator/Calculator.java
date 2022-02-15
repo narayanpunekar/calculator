4a376085a24161e31cc43ef051a759fd411ffaa1
Admin <admin@example.com>
2022-02-15 13:56:42 +0530

    Business logic, Web service controller
    Business logic, Web service controller

---
+++ src/main/java/com/narayanpunekar/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.narayanpunekar.calculator;
+
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author hp
+ */
+@Service
+public class Calculator {
+    int sum(int a, int b) {
+        return a+b;
+    }
+}
---
+++ src/main/java/com/narayanpunekar/calculator/CalculatorController.java
@@ -0,0 +1,22 @@
+package com.narayanpunekar.calculator;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author hp
+ */
+@RestController
+public class CalculatorController {
+    @Autowired
+    private Calculator calculator;
+    
+    @RequestMapping("/add")
+    String sum(@RequestParam("a") Integer a, 
+                @RequestParam("b") Integer b) {
+        return String.valueOf(calculator.sum(a, b));
+    }
+}
